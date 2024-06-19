6bcee0734a0d87071d6f893d00de93cd1404b06d
FERHAT <73959099+ferhatseker180@users.noreply.github.com>
2024-06-19 16:25:40 +0300

    This is a Spring Boot Security Application. At the moment the basic classical encryption and auth method is used. BCrypt encryption will be used in the next phase.

--- src/main/java/com/ferhat/springboot/crud/security/SecurityConfig.java
+++ src/main/java/com/ferhat/springboot/crud/security/SecurityConfig.java
@@ -2,26 +2,30 @@ package com.ferhat.springboot.crud.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager(){
+    public InMemoryUserDetailsManager userDetailsManager() {
 
         UserDetails ferhat = User.builder()
                 .username("ferhat")
                 .password("{noop}123456789")
-                .roles("ADMIN","MANAGER","EMPLOYEE")
+                .roles("ADMIN", "MANAGER", "EMPLOYEE")
                 .build();
 
         UserDetails ali = User.builder()
                 .username("ali")
                 .password("{noop}1234")
-                .roles("MANAGER","EMPLOYEE")
+                .roles("MANAGER", "EMPLOYEE")
                 .build();
 
         UserDetails cemre = User.builder()
@@ -31,5 +35,25 @@ public class SecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(ferhat, ali, cemre);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET, "/api/employee").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "/api/employee/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "/api/employee").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "/api/employee").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "/api/employee/**").hasRole("ADMIN")
+        );
+        // Use HTT Basic Authentication
+        http.httpBasic(Customizer.withDefaults());
+
+        // Disable Cross Site Request Forgery (CSRF)
+        http.csrf(csrf -> csrf.disable());
+        return http.build();
+
     }
 }
